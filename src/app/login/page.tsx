82535b8106a4ecca363880eb80e4ed99ad843f48
Jayson Lim <62184536+xiansheng31@users.noreply.github.com>
2026-05-22 00:37:23 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,66 @@
 'use client'
 
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { supabase } from '@/lib/supabase'
+
 export default function LoginPage() {
+  const router = useRouter()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  async function login() {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      alert(error.message)
+      return
+    }
+
+    router.push('/home')
+  }
+
   return (
-    <div style={{ padding: "50px" }}>
-      <h1>TEST PAGE 🔥🔥🔥</h1>
+    <div style={{ padding: '40px', maxWidth: '500px' }}>
+      <h1>Login Page</h1>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+        style={{
+          width:'100%',
+          padding:'15px',
+          marginTop:'20px'
+        }}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
+        style={{
+          width:'100%',
+          padding:'15px',
+          marginTop:'20px'
+        }}
+      />
 
       <button
-        onClick={() => alert("WORKING")}
+        onClick={login}
         style={{
-          padding: "20px",
-          background: "red",
-          color: "white"
+          width:'100%',
+          padding:'15px',
+          marginTop:'20px',
+          cursor:'pointer'
         }}
       >
-        CLICK ME
+        Login
       </button>
     </div>
   )
