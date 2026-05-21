5643675e04a1c71fd093cf51de71ddcdd30a6f6f
Jayson Lim <62184536+xiansheng31@users.noreply.github.com>
2026-05-21 23:41:04 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,77 +1,21 @@
-'use client'
+async function handleLogin(e: React.FormEvent) {
+  e.preventDefault()
 
-import { useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { createClient } from '@supabase/supabase-js'
+  console.log("Trying login...")
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  })
 
-export default function LoginPage() {
-  const router = useRouter()
+  console.log("DATA:", data)
+  console.log("ERROR:", error)
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
-
-  async function handleLogin(e: React.FormEvent) {
-    e.preventDefault()
-
-    setLoading(true)
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-
-    setLoading(false)
-
-    if (error) {
-      alert(error.message)
-      return
-    }
-
-    router.push('/home')
+  if (error) {
+    alert(error.message)
+    return
   }
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-100">
-      <form
-        onSubmit={handleLogin}
-        className="bg-white p-8 rounded-lg shadow-md w-[400px]"
-      >
-        <h1 className="text-2xl font-bold mb-6 text-center">
-          Login Page
-        </h1>
-
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
-          className="w-full p-3 border rounded mb-4"
-          required
-        />
-
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e)=>setPassword(e.target.value)}
-          className="w-full p-3 border rounded mb-6"
-          required
-        />
-
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full bg-blue-600 text-white p-3 rounded hover:bg-blue-700 cursor-pointer"
-        >
-          {loading ? 'Logging in...' : 'Login'}
-        </button>
-      </form>
-    </div>
-  )
+  alert("Login successful")
+  router.push('/home')
 }
