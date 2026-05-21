dff33357a127a4876aecdc7b9b8d3a6241aef026
Jayson Lim <62184536+xiansheng31@users.noreply.github.com>
2026-05-22 00:46:57 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,20 @@
 import { createBrowserClient } from '@supabase/ssr'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 
 const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+
+const serviceRoleKey =
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
 
 export const supabase = createBrowserClient(
   supabaseUrl,
   supabaseAnonKey
+)
+
+export const supabaseAdmin = createClient(
+  supabaseUrl,
+  serviceRoleKey
 )
